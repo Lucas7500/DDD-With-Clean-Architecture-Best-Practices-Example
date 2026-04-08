22e6da94af48a5dba60a7720ad01f212e2776a50
Lucas Iglesias <lucasiglesias2704@gmail.com>
2026-04-08 19:08:26 -0300

    build(tests): letting application internal classes visible to test projects

---
+++ src/BookStore.Application/InternalsVisibleTo.cs
@@ -0,0 +1,5 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("BookStore.Tests.xUnit")]
+[assembly: InternalsVisibleTo("BookStore.Tests.NUnit")]
+[assembly: InternalsVisibleTo("BookStore.Tests.MSTest")]
